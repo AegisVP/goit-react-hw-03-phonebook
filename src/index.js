3e7b3b55a06733a8fc64af97b9ce0a5cec1d15c6
Vladislav Pysarenko <vlad@pysarenko.com>
2022-08-11 01:43:23 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
+
+///////////////////
+// Маршрутизация //
+///////////////////
+//
+// Если приложение использует библиотеку react-router-dom для маршрутизации,
+// необходимо дополнительно настроить компонент < BrowserRouter >,
+// передав в пропе basename точное название твоего репозитория.
+// Слеши в начале и конце строки обязательны.
+//
+// <BrowserRouter basename="/_____repo_name______/">
+//   <App />
+// </BrowserRouter>
+//
+///////////////////
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
